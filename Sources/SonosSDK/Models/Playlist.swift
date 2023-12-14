ea9debe80f56d6c8497cb2355ac4af4aa9209b67
₥ia <mia.shopgal@gmail.com>
2023-12-14 14:39:44 +0800

    add type in Playlist

--- Sources/SonosSDK/Models/Playlist.swift
+++ Sources/SonosSDK/Models/Playlist.swift
@@ -12,6 +12,7 @@ public struct Playlist: Identifiable {
 
     public var id: String
     public var name: String
+    public var type: String
     public var trackCount: Int
 
     init?(_ data: Any) {
@@ -19,12 +20,14 @@ public struct Playlist: Identifiable {
         let json = JSON(data)
         guard let id = json["id"].string,
               let name = json["name"].string,
+              let type = json["type"].string,
               let trackCount = json["trackCount"].int else {
             return nil
         }
 
         self.id = id
         self.name = name
+        self.type = type
         self.trackCount = trackCount
 
     }
